7159f393d5c204112fe31c5bf91ba264ff5a39f2
hyeonseop2080 <gustjq2080@naver.com>
2023-08-07 07:45:36 +0900

    Feat: 칭찬홈 페이지 임시 제작 header, navbar, sidebar 및 팝업 확인 위해 임시 페이지 제작

---
+++ src/pages/Home.tsx
@@ -0,0 +1,72 @@
+import React from "react";
+import { useState } from "react";
+import Header from "../components/Header";
+import Navbar from "../components/Navbar";
+import Popup1 from "../components/Popup1";
+import Popup2 from "../components/Popup2";
+import PopupComplaint from "../components/PopupComplaint";
+
+const Home = () => {
+
+    const [popupState, setPopupState] = useState<number>(0);
+
+    const handlePopup1Click = () => {
+        setPopupState(1);
+    };
+
+    const handlePopup2Click = () => {
+        setPopupState(2);
+    };
+
+    const handlePopupComplaintClick = () => {
+        setPopupState(3);
+    };
+
+    return(
+        <>
+            <Header type='common'></Header>
+            <div style={{ height: '20px' }}></div>
+            <div
+                onClick={handlePopup1Click}
+                style={{ height: '535px', margin: '0 20px 0 20px',
+                        borderRadius: '16px',
+                        background: 'linear-gradient(135deg, #2A99FF 0%, #ABD7FF 100%)'
+            }}></div>
+            <div style={{ height: '40px' }}></div>
+            <div
+                onClick={handlePopup2Click}
+                style={{ height: '535px', margin: '0 20px 0 20px',
+                        borderRadius: '16px',
+                        background: 'linear-gradient(135deg, #2A99FF 0%, #ABD7FF 100%)'
+            }}></div>
+            <div style={{ height: '40px' }}></div>
+            <div
+                onClick={handlePopupComplaintClick}
+                style={{ height: '535px', margin: '0 20px 0 20px',
+                        borderRadius: '16px',
+                        background: 'linear-gradient(135deg, #2A99FF 0%, #ABD7FF 100%)'
+            }}></div>
+            <div style={{ height: '40px' }}></div>
+            <div style={{ height: '535px', margin: '0 20px 0 20px',
+                        borderRadius: '16px',
+                        background: 'linear-gradient(135deg, #2A99FF 0%, #ABD7FF 100%)'
+            }}></div>
+            <Navbar></Navbar>
+
+            {popupState === 1 && (
+                <Popup1 func={setPopupState}></Popup1>
+            )}
+
+            {popupState === 2 && (
+                <Popup2 func={setPopupState}></Popup2>
+            )}
+
+            {popupState === 3 && (
+                <PopupComplaint></PopupComplaint>
+            )}
+
+        </>
+    );
+}
+
+export default Home;
