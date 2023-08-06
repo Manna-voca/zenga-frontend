4f4464d2be5519471f5a5aaa08b8bce2a81934e7
dbstjrals <soekmin1998@naver.com>
2023-08-07 06:20:00 +0900

    Feat: 채널 홈 페이지 경로 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 
 import Onboarding from "./pages/Onboarding";
+import ChannelHome from "./pages/ChannelHome";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Onboarding />}></Route>
+      <Route path="/channel-home" element={<ChannelHome />}></Route>
     </Routes>
   );
 }
