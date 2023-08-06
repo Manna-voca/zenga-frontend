c85e92ebd78b6811a7144d21690ef581f5019d89
hyeonseop2080 <gustjq2080@naver.com>
2023-08-07 06:46:09 +0900

    Feat: 페이지 경로 추가 채널 생성페이지 및 여러 페이지 경로 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import {Routes, Route} from 'react-router-dom';
+import Login from "./pages/Login";
+import CreateChannel from "./pages/CreateChannel";
+import NewChannelOnboarding from "./pages/NewChannelOnboarding";
+import OldChannelOnboarding from "./pages/OldChannelOnboarding";
+import Home from "./pages/Home";
 
 function App() {
-  return <div>Zenga Env Setting ...</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Login />}></Route>
+      <Route path="/createchannel" element={<CreateChannel />}></Route>
+      <Route path="/createchannel/newonboarding" element={<NewChannelOnboarding />}></Route>
+      <Route path="/createchannel/oldonboarding" element={<OldChannelOnboarding />}></Route>
+      <Route path="/home" element={<Home />}></Route>
+    </Routes>
+  );
 }
 
 export default App;
