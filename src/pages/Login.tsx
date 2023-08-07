9a8677d04c4cf814b092478d0fd8a8e58512a763
dbstjrals <soekmin1998@naver.com>
2023-08-07 23:30:43 +0900

    Fix: merge conflict 수정

---
+++ src/pages/Login.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+const Login = () => {
+    const navigate = useNavigate();
+
+    const goCreateChannel = () => {
+        navigate('/createchannel');
+    };
+
+    return(
+        <button onClick={goCreateChannel}>로그인하세요</button>
+    );
+}
+
+export default Login;
