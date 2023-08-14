9053c93f4a059f6e7859f9b9ad7223cbe6f47bba
dbstjrals <soekmin1998@naver.com>
2023-08-14 17:54:09 +0900

    Feat: input type number 컴포넌트 추가
    모임 생성 중 참여 인원을 받기 위해 만듦
    숫자 외에 입력을 막았음
    일단 최대 인원 20명으로 임의 설정 해뒀음

---
+++ src/components/InputNumber.tsx
@@ -0,0 +1,110 @@
+/** @jsxImportSource @emotion/react */
+import { jsx, css } from "@emotion/react";
+import { FC } from "react";
+import { color } from "../styles/color";
+import { typography } from "../styles/typography";
+
+interface InputNumberProps {
+  isNecessary: boolean;
+  label: string;
+  placeholder: string;
+  value: string;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const InputNumber: FC<InputNumberProps> = ({
+  isNecessary,
+  label,
+  placeholder,
+  value,
+  onChange,
+}: InputNumberProps) => {
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    let inputValue = parseInt(event.target.value, 10);
+
+    if (isNaN(inputValue)) {
+      inputValue = 0; // 숫자가 아닌 입력을 0으로 처리
+    }
+
+    if (inputValue > 20) {
+      inputValue = 20; // 20 이상의 값은 20으로 제한
+    }
+
+    onChange({
+      ...event,
+      target: {
+        ...event.target,
+        value: inputValue.toString(), // 변경된 값을 문자열로 변환하여 전달
+      },
+    });
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        gap: "4px",
+        alignItems: "flex-start",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          gap: "2px",
+          alignItems: "center",
+          height: "21px",
+        }}
+      >
+        <div
+          style={{
+            ...typography.body2Medium,
+            color: `${color.onSurfaceDefault}`,
+          }}
+        >
+          {label}
+        </div>
+        <div
+          style={{
+            display: isNecessary ? "" : "none",
+            color: `${color.primary500}`,
+            height: "16px",
+            fontSize: "12px",
+            fontWeight: "500",
+            lineHeight: "100%",
+          }}
+        >
+          *
+        </div>
+      </div>
+      <input
+        type="number"
+        placeholder={placeholder}
+        css={inputTextStyle}
+        style={{ ...typography.body2Medium }}
+        value={value}
+        onChange={handleInputChange}
+        max={20}
+      />
+    </div>
+  );
+};
+
+const inputTextStyle = css`
+  font-family: Pretendard;
+  width: 100%;
+  height: 44px;
+  box-sizing: border-box;
+  padding: 10px 16px;
+  border: 1px solid ${color.outline};
+  border-radius: 8px;
+  color: ${color.onSurfaceActive};
+  &::placeholder {
+    color: ${color.onSurfaceMuted};
+  }
+  &:focus {
+    border-color: ${color.primary500};
+  }
+`;
+
+export default InputNumber;
